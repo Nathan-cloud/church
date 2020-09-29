459224457d35813fa4c4e44c9da14f4b6f089ee5
Nathan-cloud <nathanmorgan867@gmail.com>
2020-09-28 23:04:49 -0700

    design

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,7 +317,7 @@ AOS.init({
 	});
 
 	function makeTimer() {
-		var endTime = new Date('21 December 2020 10:00:00 GMT+01:00');
+		var endTime = new Date('15 October 2020 ');
 		endTime = Date.parse(endTime) / 1000;
 
 		var now = new Date();
@@ -327,7 +327,7 @@ AOS.init({
 
 		var days = Math.floor(timeLeft / 86400);
 
-		$('#days').html(days + '<span>Days</span>');
+		$('#days').html(days + '<span class="text-black">Days</span>');
 	}
 
 	setInterval(function () {
